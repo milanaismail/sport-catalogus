dffc353123c1d80c7104e4014bc1a50a9a87a28a
milanaismail <133760937+milanaismail@users.noreply.github.com>
2024-01-02 21:36:03 +0100

    api toegevoegd

---
+++ config/element-api.php
@@ -0,0 +1,45 @@
+<?php
+
+use craft\elements\Entry;
+use craft\elements\Asset;
+use craft\helpers\UrlHelper;
+
+return [
+    'endpoints' => [
+        '/api/products' => function() {
+            return [
+                'elementType' => Entry::class,
+                'criteria' => ['section' => 'products'],
+                'cache' => false,
+                'serializer' => 'jsonFeed',
+                'transformer' => function(Entry $entry) {
+                    return [
+                        'id' => $entry->id,
+                        'title' => $entry->title,
+                        'category' => $entry->productCategory,
+                        'price' => $entry->productPrice,
+                        'productImage' => str_replace("https", "http", $entry->productImage->one()->getUrl('productImage')),
+                    ];
+                },
+            ];
+        },
+        '/api/products/<entryId:\d+>' => function($entryId) {
+            return [
+                'elementType' => Entry::class,
+                'criteria' => ['id' => $entryId],
+                'one' => true,
+                'cache' => false,
+                'serializer' => 'jsonFeed',
+                'transformer' => function(Entry $entry) {
+                  return [
+                    'id' => $entry->id,
+                    'title' => $entry->title,
+                    'category' => $entry->productCategory,
+                    'price' => $entry->productPrice,
+                    'productImage' => str_replace("https", "http", $entry->productImage->one()->getUrl('productImage')),
+                  ];
+              },
+            ];
+        },
+    ]
+];
